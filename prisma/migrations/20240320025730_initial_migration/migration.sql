a06646ed8eaec1cb7839d04b234e769bc2166b95
EdwardLim1994 <edwardlimkoksiong1994@gmail.com>
2024-03-20 10:57:51 +0800

    Database schema & migration

---
+++ prisma/migrations/20240320025730_initial_migration/migration.sql
@@ -0,0 +1,101 @@
+-- CreateTable
+CREATE TABLE "users" (
+    "cuid" TEXT NOT NULL,
+    "username" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "bio" TEXT,
+    "profile" TEXT,
+    "cover" TEXT,
+    "reference" TEXT NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "users_pkey" PRIMARY KEY ("cuid")
+);
+
+-- CreateTable
+CREATE TABLE "posts" (
+    "cuid" TEXT NOT NULL,
+    "images" TEXT[],
+    "location" TEXT NOT NULL,
+    "content" TEXT NOT NULL,
+    "tags" TEXT[] DEFAULT ARRAY[]::TEXT[],
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+    "user_id" TEXT NOT NULL,
+
+    CONSTRAINT "posts_pkey" PRIMARY KEY ("cuid")
+);
+
+-- CreateTable
+CREATE TABLE "post_likes" (
+    "cuid" TEXT NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "user_id" TEXT NOT NULL,
+    "post_id" TEXT NOT NULL,
+
+    CONSTRAINT "post_likes_pkey" PRIMARY KEY ("cuid")
+);
+
+-- CreateTable
+CREATE TABLE "post_shares" (
+    "cuid" TEXT NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "user_id" TEXT NOT NULL,
+    "post_id" TEXT NOT NULL,
+
+    CONSTRAINT "post_shares_pkey" PRIMARY KEY ("cuid")
+);
+
+-- CreateTable
+CREATE TABLE "post_comments" (
+    "cuid" TEXT NOT NULL,
+    "content" TEXT NOT NULL,
+    "user_id" TEXT NOT NULL,
+    "post_id" TEXT NOT NULL,
+
+    CONSTRAINT "post_comments_pkey" PRIMARY KEY ("cuid")
+);
+
+-- CreateTable
+CREATE TABLE "post_comment_replies" (
+    "cuid" TEXT NOT NULL,
+    "content" TEXT NOT NULL,
+    "user_id" TEXT NOT NULL,
+    "comment_id" TEXT NOT NULL,
+
+    CONSTRAINT "post_comment_replies_pkey" PRIMARY KEY ("cuid")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_username_key" ON "users"("username");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_reference_key" ON "users"("reference");
+
+-- AddForeignKey
+ALTER TABLE "posts" ADD CONSTRAINT "posts_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "users"("cuid") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "post_likes" ADD CONSTRAINT "post_likes_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "users"("cuid") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "post_likes" ADD CONSTRAINT "post_likes_post_id_fkey" FOREIGN KEY ("post_id") REFERENCES "posts"("cuid") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "post_shares" ADD CONSTRAINT "post_shares_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "users"("cuid") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "post_shares" ADD CONSTRAINT "post_shares_post_id_fkey" FOREIGN KEY ("post_id") REFERENCES "posts"("cuid") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "post_comments" ADD CONSTRAINT "post_comments_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "users"("cuid") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "post_comments" ADD CONSTRAINT "post_comments_post_id_fkey" FOREIGN KEY ("post_id") REFERENCES "posts"("cuid") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "post_comment_replies" ADD CONSTRAINT "post_comment_replies_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "users"("cuid") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "post_comment_replies" ADD CONSTRAINT "post_comment_replies_comment_id_fkey" FOREIGN KEY ("comment_id") REFERENCES "post_comments"("cuid") ON DELETE CASCADE ON UPDATE CASCADE;
