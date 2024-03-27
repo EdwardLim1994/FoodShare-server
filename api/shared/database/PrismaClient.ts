2a46c6e76c23683f9965babc36e7c9f19085b77e
EdwardLim1994 <edwardlimkoksiong1994@gmail.com>
2024-03-27 11:56:28 +0800

    Create base instance rather then self instance

--- api/shared/database/PrismaClient.ts
+++ api/shared/database/PrismaClient.ts
@@ -3,13 +3,11 @@ import { PrismaClient as PCOG } from "@prisma/client";
 export default class PrismaClient {
 	private static client: PrismaClient;
 
-	private constructor() {
-		return new PCOG();
-	}
+	private constructor() {}
 
 	static instance() {
 		if (PrismaClient.client == null) {
-			PrismaClient.client = new PrismaClient();
+			PrismaClient.client = new PCOG();
 		}
 
 		return PrismaClient.client;
--- api/shared/database/SupabaseClient.ts
+++ api/shared/database/SupabaseClient.ts
@@ -3,13 +3,14 @@ import { createClient } from "@supabase/supabase-js";
 export default class SupabaseClient {
 	private static client: SupabaseClient;
 
-	private constructor() {
-		return createClient(`https://${Bun.env.URL}`, Bun.env.PUBLIC_KEY);
-	}
+	private constructor() {}
 
 	static instance() {
 		if (SupabaseClient.client == null) {
-			SupabaseClient.client = new SupabaseClient();
+			SupabaseClient.client = createClient(
+				`https://${Bun.env.SUPABASE_URL}`,
+				Bun.env.SUPABASE_PUBLIC_KEY
+			);
 		}
 
 		return SupabaseClient.client;
