34bcd476daf7fea21685ca0a59121dc58ee1bdf0
EdwardLim1994 <edwardlimkoksiong1994@gmail.com>
2024-03-27 12:30:41 +0800

    Added postController

--- api/index.ts
+++ api/index.ts
@@ -1,11 +1,13 @@
 import { Elysia } from "elysia";
 import { forEach } from "lodash";
 import userController from "./users/controller";
+import postController from "./posts/controller";
 
 let setup = new Elysia();
 
 const controllers = {
 	"/user": userController,
+	"/post": postController,
 };
 
 forEach(controllers, (controller, index) => {
