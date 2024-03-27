96ba175ccf5caff1ab807d5a8f0d843fbeaed5ba
EdwardLim1994 <edwardlimkoksiong1994@gmail.com>
2024-03-27 11:57:26 +0800

    Base service class

--- api/shared/utils/Service.ts
+++ api/shared/utils/Service.ts
@@ -1,5 +1,9 @@
 import PrismaClient from "../database/PrismaClient";
+import SupabaseClient from "../database/SupabaseClient";
 
 export default abstract class Service {
-	constructor(protected prisma: PrismaClient = PrismaClient.instance()) {}
+	constructor(
+		protected prisma: PrismaClient = PrismaClient.instance(),
+		protected supabase: SupabaseClient = SupabaseClient.instance()
+	) {}
 }
